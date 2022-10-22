16c673c6d179ff7fe69b31bf727f6fb0942d783f
ArturIshmaev <arturishmaev@yandex.ru>
2022-10-22 20:57:42 +0500

    First task

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -1,1 +1,116 @@
-﻿
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая 
+//упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+
+Console.WriteLine("введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns];
+FillArray(array);
+Console.WriteLine();
+Console.WriteLine("Массив случайных чисел: ");
+PrintArray(array);
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        for (int k = 0; k < array.GetLength(1) - 1; k++)
+        {
+            if (array[i, k] < array[i, k + 1])
+            {
+                int temp = 0;
+                temp = array[i, k];
+                array[i, k] = array[i, k + 1];
+                array[i, k + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Массив отсортированый по убыванию :");
+PrintArray(array);
+
+
+
+
+
+// System.Console.WriteLine("Введите количество строк :");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите количество столбцов :");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// int[,] array = new int[rows, columns];
+
+// FillArray(array);
+// PrintArray(array);
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         if (array[i,j] > array[i, j + 1])
+//         {
+//             int temp = array[i, j + 1];
+//             array[i, j + 1] = array[i, j];
+//             array[i, j] = temp;
+//         }
+//     }
+//     System.Console.WriteLine();
+// }
+// System.Console.WriteLine();
+// PrintArray(array);
+
+
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
