75a1d155d8988863f33c672e8be60a75413ec4a1
ArturIshmaev <arturishmaev@yandex.ru>
2022-10-22 21:15:14 +0500

    Second task

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -9,7 +9,7 @@
 // 9 5 3 2
 // 8 4 4 2
 
-void FillArray(int[,] array)
+/*void FillArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -64,38 +64,7 @@ for (int i = 0; i < array.GetLength(0); i++)
 }
 Console.WriteLine();
 Console.WriteLine("Массив отсортированый по убыванию :");
-PrintArray(array);
-
-
-
-
-
-// System.Console.WriteLine("Введите количество строк :");
-// int rows = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine("Введите количество столбцов :");
-// int columns = Convert.ToInt32(Console.ReadLine());
-// int[,] array = new int[rows, columns];
-
-// FillArray(array);
-// PrintArray(array);
-
-// for (int i = 0; i < array.GetLength(0); i++)
-// {
-
-//     for (int j = 0; j < array.GetLength(1); j++)
-//     {
-//         if (array[i,j] > array[i, j + 1])
-//         {
-//             int temp = array[i, j + 1];
-//             array[i, j + 1] = array[i, j];
-//             array[i, j] = temp;
-//         }
-//     }
-//     System.Console.WriteLine();
-// }
-// System.Console.WriteLine();
-// PrintArray(array);
-
+PrintArray(array);*/
 
 
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
@@ -107,6 +76,64 @@ PrintArray(array);
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+void MinSumElements(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка");
+}
+Console.WriteLine("введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns];
+FillArray(array);
+Console.WriteLine();
+Console.WriteLine("Массив случайных чисел: ");
+PrintArray(array);
+System.Console.WriteLine("");
+MinSumElements(array);
 
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 // Например, на выходе получается вот такой массив:
